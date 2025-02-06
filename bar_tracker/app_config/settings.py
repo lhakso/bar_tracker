ab2deaebc21483a0d955e41d1d199c4294f78c45
Luke Hakso <luke.c.hakso@gmail.com>
2025-02-06 15:07:38 -0500

    removed celery import

--- bar_tracker/app_config/settings.py
+++ bar_tracker/app_config/settings.py
@@ -10,7 +10,6 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
-from celery.schedules import crontab
 from pathlib import Path
 from decouple import config
 import dj_database_url
