bf689d0a54bd010f14f74e32f59423948d1177fc
Luke Hakso <luke.c.hakso@gmail.com>
2025-03-03 21:30:26 -0500

    tasks

--- bar_tracker/app/tasks.py
+++ bar_tracker/app/tasks.py
@@ -1,5 +1,6 @@
 import os
 import django
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "app_config.settings")
 django.setup()
 import logging
