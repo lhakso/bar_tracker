7739267657849d7765122e600cedb9be44860a69
Luke Hakso <luke.c.hakso@gmail.com>
2025-03-03 21:30:56 -0500

    was missing sys import in tasks

--- bar_tracker/app/tasks.py
+++ bar_tracker/app/tasks.py
@@ -1,5 +1,7 @@
 import os
 import django
+import sys
+
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "app_config.settings")
 django.setup()
